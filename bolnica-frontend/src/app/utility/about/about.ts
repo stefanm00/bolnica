87a63403808633e76af6e70b8aee51d7d5d11243
Stefan Malic <Malicstefan00@gmail.com>
2026-06-18 03:55:00 +0200

    frontend kraj

--- bolnica-frontend/src/app/utility/about/about.ts
+++ bolnica-frontend/src/app/utility/about/about.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
-export class AboutComponent {}
+export class AboutComponent {
+}
--- bolnica-frontend/src/app/utility/author/author.ts
+++ bolnica-frontend/src/app/utility/author/author.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-author',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './author.html',
   styleUrl: './author.css'
 })
-export class AuthorComponent {}
+export class AuthorComponent {
+}
